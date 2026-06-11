45b0cdaff0812623dbe95633b4edca8ceee002a3
neeti-atvantiq <sneeti2004@gmail.com>
2026-06-11 16:18:29 +0530

    text fixed

--- src/components/sections/Solution.tsx
+++ src/components/sections/Solution.tsx
@@ -48,7 +48,7 @@ export default function Solution() {
         <div className="solution-header reveal">
           <div className="tag" style={{ justifyContent: 'center' }}>The Solution</div>
           <h2 className="h2" style={{ textAlign: 'center' }}>
-            Intercom system that solves every <em className="em">problem</em>
+            Intercom systems that solves every <em className="em">problem</em>
           </h2>
           <p
             className="body-lg"
